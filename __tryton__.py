84083f08bfaca6a6b8d3a9075922f384755ebfe5
Cédric Krier <ced@b2ck.com>
2010-02-17 14:17:05 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO': 'Calendario de tareas',
     'name_es_ES': 'Calendario de tareas',
     'name_fr_FR' : 'Tâche Calendrier',
-    'version' : '1.4.2',
+    'version' : '1.4.3',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
